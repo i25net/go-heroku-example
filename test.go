866dabdbd063c35c43e4e7546966f861148b0b3b
song <song@admin.com>
2020-11-02 13:52:15 +0800

    add test

--- test.go
+++ test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", hello)
+	http.HandleFunc("/test", hello)
 	fmt.Println("listening...")
 	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 	if err != nil {
